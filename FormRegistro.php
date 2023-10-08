d5896be6449765da24ce0d4a03c33c3fbae8b1f4
LeoSolMen <lsolis10885@ufide.ac.cr>
2023-10-08 15:11:30 -0600

    Validacion de correo en php
    Query para validar si un correo ya fue utilizado para registrarse

--- FormRegistro.php
+++ FormRegistro.php
@@ -9,17 +9,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $hashedPassword = password_hash($password, PASSWORD_BCRYPT); // Hash the password
 
-    $sql = "INSERT INTO USERS (NAME, FLASTNAME, EMAIL, PASSWORD) 
-    VALUES ('$nombre', '$primerApellido', '$email', '$hashedPassword')";
-    
-    if ($con->query($sql) === TRUE) {
-        echo "Registro exitoso.";
-    } else {
-        echo "Error al registrar: " . $con->error;
-    }
+    // Validacion de email
+    $checkEmailQuery = "SELECT * FROM USERS WHERE EMAIL = '$email'";
+    $result = $con->query($checkEmailQuery);
 
-    include 'bd_disconn.php';
-}else{
-    echo "Acceso no autorizado";
-}
+    if ($result->num_rows > 0) {
+            // Correo en uso
+            echo "El correo electrónico ya está en uso. Por favor, utilice otro correo.";
+        } else {
+            // Correo nuevo, se procede con registro
+            $sql = "INSERT INTO USERS (NAME, FLASTNAME, EMAIL, PASSWORD) 
+            VALUES ('$nombre', '$primerApellido', '$email', '$hashedPassword')";
+            
+            if ($con->query($sql) === TRUE) {
+                echo "Registro exitoso.";
+            } else {
+                echo "Error al registrar: " . $con->error;
+            }
+        }
+
+            include 'bd_disconn.php';
+        } else {
+            echo "Acceso no autorizado";
+        }
 ?>
