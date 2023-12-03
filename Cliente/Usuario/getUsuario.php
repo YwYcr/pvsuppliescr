d81154251e74a31e98fbc3b613b165e0f08e1e1c
Josh-1494 <79804411+Josh-1494@users.noreply.github.com>
2023-12-02 21:21:58 -0600

    -- Stored Procedures para Users -Clientes -- - SP creados en la Base de datos. - Se actualizan los codigos de PHP donde se hacen los llamados y se conserva tambien el codigo viejo por cualquier eventualidad, despues de testear se recomienda eliminar.

--- Cliente/Usuario/getUsuario.php
+++ Cliente/Usuario/getUsuario.php
@@ -1,12 +1,16 @@
 <?php
+
 include 'bd_conn.php';
 
-// if (isset($_GET['userId'])) {
+if (isset($_GET['userID'])) {
     $userID = $_GET['userID'];
 
-    $sql = "SELECT * FROM USERS WHERE IDUSER = $userID";
-    // $sql = "SELECT * FROM USERS WHERE IDUSER = $userId";
-    $result = $con->query($sql);
+    // Llama al procedimiento almacenado
+    $stmt = $con->prepare("CALL GetUserByID(?)");
+    $stmt->bind_param("i", $userID);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
@@ -14,10 +18,41 @@ include 'bd_conn.php';
     } else {
         echo json_encode(array('error' => 'Usuario no encontrado'));
     }
-// } else {
-//     // No se proporcionó un ID de usuario válido
-//     echo json_encode(array('error' => 'ID de usuario no válido'));
-// }
+
+    $stmt->close();
+} else {
+    // No se proporcionó un ID de usuario válido
+    echo json_encode(array('error' => 'ID de usuario no válido'));
+}
 
 include 'bd_disconn.php';
+
+/**************************************************/
+/******************** OLD CODE ********************/
+/******************** WITH NO *********************/
+/**************** STORE PROCEDURE *****************/
+/**************************************************/
+
+// include 'bd_conn.php';
+
+// // if (isset($_GET['userId'])) {
+//     $userID = $_GET['userID'];
+
+//     $sql = "SELECT * FROM USERS WHERE IDUSER = $userID";
+//     // $sql = "SELECT * FROM USERS WHERE IDUSER = $userId";
+//     $result = $con->query($sql);
+
+//     if ($result->num_rows == 1) {
+//         $row = $result->fetch_assoc();
+//         echo json_encode($row);
+//     } else {
+//         echo json_encode(array('error' => 'Usuario no encontrado'));
+//     }
+// // } else {
+// //     // No se proporcionó un ID de usuario válido
+// //     echo json_encode(array('error' => 'ID de usuario no válido'));
+// // }
+
+// include 'bd_disconn.php';
+
 ?>
