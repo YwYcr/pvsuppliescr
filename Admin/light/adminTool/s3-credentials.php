060563d99ca08a3ee150a3c203dc7bd25e64fc56
Josh-1494 <79804411+Josh-1494@users.noreply.github.com>
2023-12-10 23:30:56 -0600

    Credenciales de AWS eliminadas

--- Admin/light/adminTool/s3-credentials.php
+++ Admin/light/adminTool/s3-credentials.php
@@ -8,14 +8,7 @@ use Aws\Exception\AwsException;
 $S3Options = 
 [
 	'version' => 'latest',
-	'region'  => 'us-east-1',
-	'credentials' => 
-	[
-		// 'key' => 'AKIAVBEKFJYX4SC53FAI',
-		// 'secret' => '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'
-		'key' => 'AKIAVBEKFJYXYNV6IGHU',
-		'secret' => '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'
-	]
+	'region'  => 'us-east-1'
 ]; 
 
 $s3 = new S3Client($S3Options); 
